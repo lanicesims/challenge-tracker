fa602359758de120bc3fafb4535b39c99d963e02
Jenessa White <jenessaw31@gmail.com>
2018-11-27 18:23:22 -0600

    Pages!

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PasswordForgetPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>PasswordForgetPage</h1>
+      </div>
+    );
+  }
+}
+
+export default PasswordForgetPage;
---
+++ src/components/SignIn/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignInPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>SignInPage</h1>
+      </div>
+    );
+  }
+}
+
+export default SignInPage;
