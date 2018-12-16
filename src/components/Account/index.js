e49a28234afd8000412886592d71318ebb5218f1
Jenessa White <jenessaw31@gmail.com>
2018-12-16 17:57:49 -0600

    password change form

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import PasswordChange from '../PasswordChange';
+
 class AccountPage extends React.Component {
   render() {
     return (
       <div>
         <h1>Account</h1>
+        <PasswordChange />
       </div>
     );
   }
--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
-class PasswordChangePage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>PasswordChangePage</h1>
-      </div>
-    );
-  }
-}
+import PasswordChangeForm from './PasswordChangeForm';
+
+const PasswordChangePage = () => (
+  <Container>
+    <Row className="text-center">
+      <Col sm="12">
+        <h1>Change your Password</h1>
+      </Col>
+    </Row>
+    <Row className="justify-content-center">
+      <Col sm="12" md="8">
+        <p>
+          <strong>New password, who dis?</strong>
+          <br/>
+          Change your password below and be on your way
+        </p>
+        <PasswordChangeForm />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default PasswordChangePage;
